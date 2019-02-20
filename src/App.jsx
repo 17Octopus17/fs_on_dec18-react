a48db837b8dcd8dcef8e96766f674746a71a1b39
mgrinko <glukym@gmail.com>
2019-02-20 11:46:58 +0200

    Cells are now filled each second

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,39 @@ class Field extends React.Component {
             value, id: index
           };
         }),
-    }
+    };
+  }
+
+  componentDidMount() {
+    let position = 0;
+
+    const timer = setInterval(() => {
+      this.setState(state => {
+        const newCells = [...state.cells];
+        const oldCell = newCells[position];
+
+        newCells[position] = {
+          ...oldCell,
+          value: (position % 2 === 0) ? 'X' : '0',
+        };
+
+        position++;
+
+        if (position >= state.cells.length) {
+          clearInterval(timer);
+        }
+
+        return {
+          cells: newCells
+        };
+      });
+    }, 1000);
   }
 
   render() {
     const { cells } = this.state;
     const { size } = this.props;
-    const cellWidth = 100
+    const cellWidth = 100;
 
     return (
       <div className="field" style={{ width: size * cellWidth }}>
